634bc323d7ec64cd7a84cca0a73f01043f1216a5
Sean Joyce <seanthomasjoyce@icloud.com>
2025-07-01 23:09:25 +0100

    feat: add Card component and integrate into home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,65 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <section className="border rounded p-4 max-w-md ">
-      Sean Joyce
-      <div>Frontend Developer</div>
-    </section>
+    <div className="flex flex-col gap-4">
+      <section className=" p-4 max-w-lg">
+        <h1 className="text-6xl">Sean Joyce</h1>
+        <h2 className="text-lg">Software Engineer</h2>
+      </section>
+
+      <section className="max-w-full p-4">
+        <div className="mb-2 text-2xl">My Work</div>
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
+    </div>
   );
 }
